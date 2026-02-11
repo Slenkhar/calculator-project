a5ecd753972295ea96e01b76bab1e83ed9ace319
David Secretain <slenkhar@gmail.com>
2026-02-11 17:54:35 +0000

    Create math and operate functions
    basic functions to take in the operator, num1 and num2 variables to return a result

---
+++ javascript.js
@@ -0,0 +1,46 @@
+let num1 = "";
+let num2 = "";
+let operator = "";
+
+//---------------
+//Math functions
+//---------------
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+//-----------------
+//Operate function
+//-----------------
+function operate(op, a, b) {
+  a = parseFloat(a);
+  b = parseFloat(b);
+  let result;
+  switch (op) {
+    case "+":
+      result = add(a, b);
+      break;
+    case "-":
+      result = subtract(a, b);
+      break;
+    case "*":
+      result = multiply(a, b);
+      break;
+    case "/":
+      result = divide(a, b);
+      break;
+  }
+  return Math.round((result * 100000) / 100000);
+}
